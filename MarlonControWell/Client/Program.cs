d6fda45a3a802cf22fe7790b846322128c30da63
SolveNaN <dagoberto6993@gmail.com>
2023-02-12 18:05:22 -0500

    Guardado con errores

--- MarlonControWell/Client/Program.cs
+++ MarlonControWell/Client/Program.cs
@@ -2,12 +2,14 @@ global using MarlonControWell.Client.Services.PozoService;
 using MarlonControWell.Client;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using CurrieTechnologies.Razor.SweetAlert2;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 builder.Services.AddScoped<IPozoService, PozoService>();
 
+builder.Services.AddSweetAlert2();
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
 await builder.Build().RunAsync();   
--- MarlonControWell/Server/Program.cs
+++ MarlonControWell/Server/Program.cs
@@ -1,7 +1,9 @@
 global using MarlonControWell.Shared;
-using MarlonControWell.Server.Data;
+global using Microsoft.EntityFrameworkCore;
+global using MarlonControWell.Server.Data;
 using Microsoft.AspNetCore.ResponseCompression;
-using Microsoft.EntityFrameworkCore;
+using MarlonControWell.Client.Services.PozoService;
+using MarlonControWell.Server.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -15,20 +17,41 @@ builder.Services.AddDbContext<DataContext>(options =>
 builder.Services.AddControllersWithViews();
 builder.Services.AddRazorPages();
 
+builder.Services.AddEndpointsApiExplorer();
+
+
+//agregado para swager
+builder.Services.AddControllers();
+
+builder.Services.AddEndpointsApiExplorer();
+
+
+
+
+
 var app = builder.Build();
+
+
+
+
+
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseWebAssemblyDebugging();
+    //Agregado para Swagger
+    app.UseSwagger();
+   
 }
 else
 {
-    app.UseExceptionHandler("/Error");
+    app.UseExceptionHandler("/Error cometido");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
+app.UseSwagger();
 app.UseHttpsRedirection();
 
 app.UseBlazorFrameworkFiles();
